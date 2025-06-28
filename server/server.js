7a6c111322bee972bd39d72bfb3339950cd7d883
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-06-28 14:53:13 +0600

    Backend Server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+// Middleware to parse JSON requests
+app.use(express.json());
+
 app.get('/', (req, res) => res.send("API is Working"));
 
 app.listen(PORT, () => {
