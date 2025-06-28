04b9a1fbdceea55685984f8e1e8f3571a2838a60
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-06-28 14:48:09 +0600

    Backend Server

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => res.send("API is Working"));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
