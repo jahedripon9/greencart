51f2f99c4216890a35d5c4da2cf539b398669dd3
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-06-30 11:30:59 +0600

    Create Register & login Api nand Testing

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -35,3 +35,38 @@ export const register = async (req, res) => {
         res.json({ success: false, message: error.message });
     }
 };
+
+// Login User: /api/user/login
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password)
+            return res.json({ success: false, message: "Email and Password are required" });
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.json({ success: false, message: "Invalid email or Password" });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)
+        if (!isMatch)
+            return res.json({ success: false, message: "Invalid Email or Password" });
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        res.cookie("token", token, {
+            httpOnly: true, // Prevents JavaScript access to the cookie
+            secure: process.env.NODE_ENV === 'production', // Use secure cookies in production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // Adjust sameSite policy based on environment
+            maxAge: 7 * 24 * 60 * 60 * 1000 // Cookie expiration time (7 days)
+        });
+
+        return res.json({ success: true, user: { email: user.email, name: user.name } })
+
+
+    } catch (error) {
+        console.error(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
